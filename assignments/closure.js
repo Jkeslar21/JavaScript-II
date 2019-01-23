9f202f11c6ce3ae19395b09de1b6293394636712
Jkeslar21 <46494969+Jkeslar21@users.noreply.github.com>
2019-01-22 20:18:53 -0700

    afterhoursHELPED!

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,12 +1,32 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function favActivity(activity){
+  const fa = activity
+  console.log(`I LOVE to play ${fa}!`);
+  function beginner(){
+      console.log(`Even as a beginner, I LOVED to play the ${fa}!`);
+      function advanced(){
+          console.log(`One thing i've come to appreciate over the years, is the magic that lies within an Acoustic ${fa}!`);
+      } advanced();
+  } 
+  beginner()
+}
+favActivity('Guitar');
 
 // ==== Challenge 2: Create a counter function ====
-**super confused with this** will try again later** 
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+const counter = (function(count) {
+  return function () {
+    count+=1;
+    return count;
+  }
+}(0));
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+ // Return a function that when invoked increments and returns a counter variable. 
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
